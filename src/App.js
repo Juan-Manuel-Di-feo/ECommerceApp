74098b0b9dfb43c6839d887dff8ac3d181d601bb
Juan-Manuel-Di-Feo <difeojuanmanuel@gmail.com>
2021-06-29 02:58:44 -0300

    added shop menu with items preview in each section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 import {Switch, Route} from 'react-router-dom'
+import ShopPage from './pages/shop-page/shop-page.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop' component={ShopPage}/>
       </Switch>
     </div>
   );
