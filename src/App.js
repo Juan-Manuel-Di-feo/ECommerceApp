15f3599fbf74376648de3ab855b1d72ff9bff2cb
Juan-Manuel-Di-Feo <difeojuanmanuel@gmail.com>
2021-07-03 22:35:36 -0300

    Added Auth page and sign in component (non functional yet)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/homepage.component';
 import {Switch, Route} from 'react-router-dom'
 import ShopPage from './pages/shop-page/shop-page.component';
 import Header from './components/header-component/header.component';
+import AuthPage from './pages/auth-page/auth-page';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
+        <Route exact path='/auth' component={AuthPage}/>
       </Switch>
     </div>
   );
